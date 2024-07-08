f67eebb282fc8efa0f6d481c3214c4004512a0de
SHIHAB4545W <shihab.wael4545@gmail.com>
2024-07-08 15:15:53 +0300

    edit_back

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 const multer = require("multer");
 const fs = require("fs");
 const mime = require("mime-types");
+const { log } = require("console");
+const { dirname } = require("path");
 
 require("dotenv").config();
 const app = express();
@@ -22,7 +24,7 @@ const bucket = "dawid-booking-app";
 
 app.use(express.json());
 app.use(cookieParser());
-app.use("/uploads", express.static(__dirname + "/uploads"));
+app.use("/uploads", express.static("E:/airbnb-clone-master/api" + "/uploads/"));
 app.use(
   cors({
     credentials: true,
@@ -30,28 +32,28 @@ app.use(
   })
 );
 
-async function uploadToS3(path, originalFilename, mimetype) {
-  const client = new S3Client({
-    region: "us-east-1",
-    credentials: {
-      accessKeyId: process.env.S3_ACCESS_KEY,
-      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-    },
-  });
-  const parts = originalFilename.split(".");
-  const ext = parts[parts.length - 1];
-  const newFilename = Date.now() + "." + ext;
-  await client.send(
-    new PutObjectCommand({
-      Bucket: bucket,
-      Body: fs.readFileSync(path),
-      Key: newFilename,
-      ContentType: mimetype,
-      ACL: "public-read",
-    })
-  );
-  return `https://${bucket}.s3.amazonaws.com/${newFilename}`;
-}
+// async function uploadToS3(path, originalFilename, mimetype) {
+//   const client = new S3Client({
+//     region: "us-east-1",
+//     credentials: {
+//       accessKeyId: process.env.S3_ACCESS_KEY,
+//       secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+//     },
+//   });
+//   const parts = originalFilename.split(".");
+//   const ext = parts[parts.length - 1];
+//   const newFilename = Date.now() + "." + ext;
+//   await client.send(
+//     new PutObjectCommand({
+//       Bucket: bucket,
+//       Body: fs.readFileSync(path),
+//       Key: newFilename,
+//       ContentType: mimetype,
+//       ACL: "public-read",
+//     })
+//   );
+//   return `https://${bucket}.s3.amazonaws.com/${newFilename}`;
+// }
 
 function getUserDataFromReq(req) {
   return new Promise((resolve, reject) => {
@@ -128,33 +130,32 @@ app.get("/api/profile", (req, res) => {
 app.post("/api/logout", (req, res) => {
   res.cookie("token", "").json(true);
 });
-
-app.post("/api/upload-by-link", async (req, res) => {
+app.post("/upload-by-link", async (req, res) => {
   const { link } = req.body;
   const newName = "photo" + Date.now() + ".jpg";
   await imageDownloader.image({
     url: link,
-    dest: "/tmp/" + newName,
+    dest: "E:/airbnb-clone-master/api" + "/uploads/" + newName,
   });
-  const url = await uploadToS3(
-    "/tmp/" + newName,
-    newName,
-    mime.lookup("/tmp/" + newName)
-  );
-  res.json(url);
+
+  res.json("E:/airbnb-clone-master/api" + "/uploads/" + newName);
 });
 
-const photosMiddleware = multer({ dest: "/tmp" });
+const photosMiddleware = multer({ dest: "/uploads" });
 app.post(
   "/api/upload",
   photosMiddleware.array("photos", 100),
   async (req, res) => {
     const uploadedFiles = [];
     for (let i = 0; i < req.files.length; i++) {
-      const { path, originalname, mimetype } = req.files[i];
-      const url = await uploadToS3(path, originalname, mimetype);
-      uploadedFiles.push(url);
+      const { path, originalname } = req.files[i];
+      const parts = originalname.split(".");
+      const ext = parts[parts.length - 1];
+      const newPath = path + "." + ext;
+      fs.renameSync(path, newPath);
+      uploadedFiles.push(newPath.replace("uploads/", ""));
     }
+    console.log(uploadedFiles);
     res.json(uploadedFiles);
   }
 );
